a9f7b04fb47c6c3dd5d394acf95a7b046ebf4bc0
zane <xXsuraazXx@gmail.com>
2026-05-06 14:54:06 +0545

    add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    int b = 10;
+    for
+    (int i = 0; i < 5; i++) {
+            printf("Iteration %d: a = %d, b = %d\n", i, a, b);
+            a += 1;
+            b += 2;
+        } 
+    return 0;
+}
